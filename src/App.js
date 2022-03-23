13112653e6606f8144773511dd47c544375c205d
bushmusi <bushera.mestofa@gmail.com>
2022-03-23 17:31:54 +0300

    Update files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import Calculator from './calculator';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          <code>
+            src/App.js
+          </code>
+          and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
