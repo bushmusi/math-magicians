9e67a42b65190591a07add65a3e3e15461c47121
Bushra Mustofa <bushera.mestofa@gmail.com>
2022-03-24 23:31:26 +0300

    remove old file

--- src/calculator.js
+++
@@ -1,9 +0,0 @@
-// import calculate from './logic/calculate';
-// import operate from './logic/operate';
-import MainContainer from './component/calculater-box';
-
-const Calculator = () => (
-  <MainContainer />
-);
-
-export default Calculator;
