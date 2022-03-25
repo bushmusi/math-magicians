f3402ab672df702790ec8338bb73501491338977
bushmusi <bushera.mestofa@gmail.com>
2022-03-25 17:26:54 +0300

    Clean code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,10 @@
-// import Calculator from './component/Calculator';
+import Calculator from './component/Calculator';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-      <div className="ResultBox InputBox">
-        <h3>0</h3>
-      </div>
-
-      <div className="AC InputBox">
-        <button type="button">AC</button>
-      </div>
-
-      <div className="InputBox">
-        <button type="button">+/-</button>
-      </div>
-
-      <div className="InputBox">
-        <button type="button">%</button>
-      </div>
-
-      <div className="InputBox LeftBtn">
-        <button type="button">/</button>
-      </div>
-
-      <div className="InputBox">
-        <button type="button">7</button>
-      </div>
-
-      <div className="InputBox">
-        <button type="button">8</button>
-      </div>
-
-      <div className="InputBox">
-        <button type="button">9</button>
-      </div>
-
-      <div className="InputBox LeftBtn">
-        <button type="button">*</button>
-      </div>
+    <div className="App" id="GridContainer">
+      <Calculator />
     </div>
   );
 }
