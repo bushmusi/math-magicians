64b30791c9817bc2ed4e30a62fcc2752939aaad8
bushmusi <bushera.mestofa@gmail.com>
2022-03-31 14:11:24 +0300

    Add home and quote component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 import Calculator from './components/pages/calculator/Calculator';
-import Header from './components/Header';
+import Home from './components/pages/Home';
+import Quote from './components/pages/Quote';
 import './App.css';
 import './App.scss';
 
@@ -8,11 +9,21 @@ function App() {
   return (
     <>
       <Switch>
-        <Route path="/">
-          <Header />
-          <div className="App" id="GridContainer">
-            <Calculator />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/calculator">
+          <div className="CalcContainer">
+            <div className="caclTitle">
+              <h2>Let as do some math!!!</h2>
+            </div>
+            <div className="App" id="GridContainer">
+              <Calculator />
+            </div>
           </div>
+        </Route>
+        <Route path="/quote">
+          <Quote />
         </Route>
       </Switch>
     </>
