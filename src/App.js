b875073c062cf8af5a623ff4a00984fb7396cf7a
Bushra Mustofa <bushera.mestofa@gmail.com>
2022-03-31 07:19:55 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import Calculator from './components/Calculator';
+import { Route, Switch } from 'react-router-dom';
+import Calculator from './components/pages/calculator/Calculator';
+import Header from './components/Header';
 import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App" id="GridContainer">
-      <Calculator />
-    </div>
+    <>
+      <Route exact path='/'>
+        <Header />
+        <div className="App" id="GridContainer">
+          <Calculator />
+        </div>
+      </Route>
+    </>
+
   );
 }
 
