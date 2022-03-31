17e03413dc3cc1b85cd8c34046dad8054d8d73c2
Bushra Mustofa <bushera.mestofa@gmail.com>
2022-03-31 07:21:27 +0300

    Move GetElements

---
+++ src/components/pages/calculator/GetElements.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import inputList from '../../../Child-Data';
+import CellComponent from './CellComponent';
+
+class GetElements extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleOperation = this.handleOperation.bind(this);
+  }
+
+  handleOperation(e) {
+    /* eslint-disable */
+    this.props.handleOperation(e);
+  }
+
+  render() {
+    const items = [];
+    Object.entries(inputList).forEach((key) => {
+      const keyClassName = key[1].isOperation === true ? 'operator' : 'number';
+      const cellInfo = {
+        cellClassName: keyClassName,
+        cellID: key[0],
+        cellSymbol: key[1].symbol,
+        isCellLeftSide: key[1].isLeft ? 'LeftBtn' : '',
+      };
+
+      items.push(<CellComponent itemInfo={cellInfo} key={`Cell${key[0]}`} operate={this.handleOperation} />);
+    });
+    return items;
+  }
+}
+
+export default GetElements;
